d9eea18caf0b509eaa486e095b370c4f407c983d
IgnacioMurubeCrego <imurubec@alumnos.nebrija.es>
2023-12-06 19:19:05 +0100

    update canMachine

--- src/canMachine.vhd
+++ src/canMachine.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -41,10 +42,34 @@ end canMachine;
 
 architecture Behavioral of canMachine is
 
+signal precio_lata : std_logic_vector(2 downto 0) := "010";
+signal counter : std_logic_vector(2 downto 0);
+signal en_resta : std_logic := '0';
+signal suma_resta : std_logic_vector(2 downto 0) := "000";
 type SM_state is (E0,E1,E2,E3,E4,E5);
 signal CS, NS : SM_state;
+ 
+component full_add_sub_Nbits is
+Generic( N : integer := 3
+        );
+Port ( clk : in std_logic;
+       A,B : in std_logic_vector(N-1 downto 0);
+       add_sub : in std_logic;
+       S : out std_logic_vector(N downto 0)
+    );
+end component;
 
 begin
+
+SUMADOR_RESTADOR : full_add_sub_Nbits
+generic map ( N => 3 )
+port map (
+          clk => clk,
+          A => counter,
+          B => suma_resta,
+          add_sub => en_resta,
+          S => counter
+          );
 
 -- Bucle Transición de Estados Maquina de Moore :
 process(clk,rst,coin_in)
@@ -102,26 +127,24 @@ begin
 end process;
 
 -- Casos por Estado :
-process(CS,coin_in)
+process(CS,coin_in,counter)
 begin
     case CS is
-        when E0 => 
-            coin_out <= "000";
-        when E1 => 
-            coin_out <= "000";
-        when E2 => 
-            coin_out <= "000";
-            lata <= '1';
-        when E3 => 
-            coin_out <= "001";
-            lata <= '1';
-        when E4 => 
-            coin_out <= "100";
-            lata <= '1';
-        when E5 => 
-            coin_out <= "011";
+        when E0 =>
+            suma_resta <= coin_in;
+            en_resta <= '0';
+            lata <= '0';
+            counter <= "000";
+        when E1 =>
+            suma_resta <= coin_in;
+            en_resta <= '0';
+            lata <= '0';
+        when others =>
+            suma_resta <= precio_lata;
+            en_resta <= '1';
             lata <= '1';
     end case;
+    coin_out <= counter;
 end process;
 
 end Behavioral;
