dafa65edad928b874098c22672f8c6f9bdae004f
IgnacioMurubeCrego <123760615+IgnacioMurubeCrego@users.noreply.github.com>
2023-12-08 12:26:51 +0100

    Functioning canMachine.vhd

--- canMachine.vhd
+++ canMachine.vhd
@@ -35,7 +35,8 @@ architecture Behavioral of canMachine is
 
 type state is (E0,E1,E2);
 signal CS,NS : state; -- Current State, Next State
-signal money_in : std_logic_vector(2 downto 0); 
+signal money_in : std_logic_vector(2 downto 0);
+--signal counter : std_logic_vector(2 downto 0);  
 signal precio_lata : unsigned(2 downto 0) := "010";
 
 begin
@@ -46,15 +47,17 @@ begin
     if rising_edge(clk) then
         if rst = '0' then
             CS <= NS;
-            money_in <= std_logic_vector(unsigned(money_in) + unsigned(coin_in));
+            --counter <= std_logic_vector(unsigned(counter) + unsigned(coin_in));
+            --report "Counter: " & integer'image(to_integer(unsigned(counter)));
         else
+            --counter <= "000";
             CS <= E0;
         end if;
     end if;
 end process;
 
 -- Salidas
-process(CS,coin_in,rst,money_in,precio_lata)
+process(CS,coin_in,rst,money_in,precio_lata)--counter)
 begin
     case CS is 
         when E0 =>
@@ -63,10 +66,13 @@ begin
             coin_out <= "000";
             if coin_in = "001" then
                 NS <= E1;
+                money_in <= "001";
             elsif coin_in = "010" then
                 NS <= E2;
+                money_in <= "010";
             elsif coin_in = "101" then
                 NS <= E2;
+                money_in <= "101";
             else
                 NS <= CS;
             end if;
@@ -80,17 +86,21 @@ begin
             end if;
             if coin_in = "001" then
                 NS <= E2;
+                money_in <= "010";
             elsif coin_in = "010" then
                 NS <= E2;
+                money_in <= "011";
             elsif coin_in = "101" then
                 NS <= E2;
+                money_in <= "110";
             else
                 NS <= CS;
-            end if;
-            
+            end if;            
          when E2 => 
             lata <= '1';
-            coin_out <= std_logic_vector(unsigned(money_in) - 2 );
+            --money_in <= counter;
+            report "Money_in: " & integer'image(to_integer(unsigned(money_in)));
+            coin_out <= std_logic_vector(unsigned(money_in) - unsigned(precio_lata));
             NS <= E0;
 
     end case;
